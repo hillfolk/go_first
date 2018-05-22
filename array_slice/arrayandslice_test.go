cf6b4d70df073b3d47e575dc190c067691aa650e
ParkJunYong <pjy1418@gmail.com>
2018-05-22 21:40:30 +0900

    array

--- array_slice/arrayandslice_test.go
+++ array_slice/arrayandslice_test.go
@@ -3,12 +3,16 @@ package arrayandslice
 import "fmt"
 
 func Example_array() {
-	fruits := [...]string{"사과","바나나","토마토"}
+	fruits := [...]string{"사과","바나나","토마토","수박"}
 	for _, fruit := range fruits {
 		fmt.Printf("%s는 맛있다.\n", fruit)	
 	}
 	// Output:
-	// 사과는 맛있다
+	// 사과는 맛있다.
 	// 바나나는 맛있다.
 	// 토마토는 맛있다.
+}
+
+func Example_slice() {
+	var 
 }
