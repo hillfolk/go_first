8a5837b976de3e3fc143e5e1c97a89af46fc4839
ParkJunYong <pjy1418@gmail.com>
2018-05-22 20:55:40 +0900

    string

--- string/string_test.go
+++ string/string_test.go
@@ -13,3 +13,15 @@ func Example_strCat() {
 	// abcdef
 	// abcdef
 }
+
+func Example_pirntBytes2(){
+	s := "가나다"
+	fmt.Printf("%x\n",s)
+	fmt.Printf("% x\n",s)
+	// Output:
+	// eab080eb8298eb8ba4
+	// ea b0 80 eb 82 98 eb 8b a4
+	
+}
+	
+	
