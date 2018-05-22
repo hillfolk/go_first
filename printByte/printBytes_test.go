0498a20680a5d553b2cb96f2092aa6607c7ced98
ParkJunYong <pjy1418@gmail.com>
2018-05-22 14:57:23 +0900

    add files

---
+++ printByte/printBytes_test.go
@@ -0,0 +1,14 @@
+package printBytes
+
+import "fmt"
+
+func Example_PrintBytes() {
+	s := "가나다"
+	for i := 0;i < len(s); i++ {
+		fmt.Printf("%x:",s[i])
+	}
+	
+	fmt.Println()
+	// Output:
+	// ea:b0:80:eb:82:98:eb:8b:a4:
+}
