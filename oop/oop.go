ad05f12751b705e73e3b380cdf6c7552b02e8898
ParkJunYong <pjy1418@gmail.com>
2018-05-26 23:16:20 +0900

    gitignore

---
+++ oop/oop.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//구조체
+type Item struct {
+	name string
+	price float64
+	quantity int
+}
+
+func (t Item) Cost() float64 {
+	return t.price * float64(t.quantity)
+}
+
+func main(){
+	shirt := Item{name:"Men's Slim-Fit Shirt", price: 25000, quantity:3}
+
+	fmt.Println(shirt.Cost())
+}
