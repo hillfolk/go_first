166e12254ae3c006b73b2d96e1e41de80bfc87a7
ParkJunYong <pjy1418@gmail.com>
2018-05-30 19:42:20 +0900

    sharedMemory

--- gorouatione/shardmemory.go
+++ gorouatione/shardmemory.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-type shareMap struct {
+type sharedMap struct {
 	m map[string]interface{}
 	c chan command
 
@@ -19,14 +19,14 @@ const (
 	set = iota
 	get
 	remove
-	cont
+	count
 )
 
-func (sm shardMap) Set(k string, v interface{}) {
+func (sm sharedMap) Set(k string, v interface{}) {
 	sm.c <- command{action: set, key: k, value: v}
 }
 
-func (sm shardMap) Get(k string) (interface{}, bool) {
+func (sm sharedMap) Get(k string) (interface{}, bool) {
 	callback := make(chan interface{})
 	sm.c <- command{action: get, key: k, result: callback}
 	result := (<-callback).([2]interface{})
@@ -37,3 +37,49 @@ func (sm sharedMap) Remove(k string) {
 	sm.c <- command{action: remove, key:k}
 }
 
+func (sm sharedMap) Count() int {
+	callback := make(chan interface{})
+	sm.c <- command{action: count, result: callback}
+	return (<-callback).(int)
+}
+
+func (sm sharedMap) run() {
+	for cmd := range sm.c {
+		switch cmd.action {
+		case set:
+			sm.m[cmd.key] = cmd.value
+		case get:
+			v, ok := sm.m[cmd.key]
+			cmd.result <- [2]interface{}{v,ok}
+		case remove:
+			delete(sm.m,cmd.key)
+		case count:
+			cmd.result <- len(sm.m)
+		}
+	}
+}
+
+func NewMap() sharedMap {
+	sm := sharedMap{
+		m: make(map[string]interface{}),
+		c: make(chan command),
+	}
+	go sm.run()
+	return sm
+}
+
+func main() {
+	m := NewMap()
+
+	m.Set("foo","bar")
+
+	t,ok := m.Get("foo")
+
+	if ok {
+		bar := t.(string)
+		fmt.Println("bar:",bar)
+	}
+
+	m.Remove("foo")
+	fmt.Println("Count",m.Count())
+}
